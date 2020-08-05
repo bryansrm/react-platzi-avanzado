ee6aa44f35920c3f843e20e570532ec149b41b02
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-05 08:09:09 -0500

    se agrega logo svg

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react'
 import { GlobalStyle } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories/index'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards/index'
+import { Logo } from './components/Logo/index'
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
---
+++ src/components/Logo/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  text-align: center;
+`
+
+export const Svg = styled.svg`
+  width: 250px;
+  margin: 0 auto;
+  margin-top: -20px;
+`
