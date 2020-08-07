95309438b589ada8594469954118a1ccccfc291d
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-06 21:46:06 -0500

    categorias fijas en cabecera

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -7,6 +7,7 @@ import { List, Item } from './styles'
 export const ListOfCategories = () => {
 
   const [ categories, setCategories] = useState([])
+  const [ showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     fetch('https://petgram-server-bryansrm.vercel.app/categories')
@@ -16,13 +17,32 @@ export const ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.addEventListener('scroll', onScroll)
+
+  })
+
+  const renderList = ( fixed ) => (
+    <List className={ fixed ? 'fixed' : ''}>
       {
         categories.map(
           category => <Item key={category.id}> <Category {...category}/> </Item>
         )
       }
     </List>
+  )
+
+  return (
+    <>
+      { renderList() }
+      { showFixed && renderList(true) }
+    </>
   )
 }
