6d80e16d6eb94573daa852f68f20c581edbb64c2
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-06 21:07:14 -0500

    se agrega hooks para el llamado del ws de las categorias

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Catergory, Category } from '../category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
+//import { categories as mockCategories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
+
+  const [ categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetch('https://petgram-server-bryansrm.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
   return (
     <List>
       {
         categories.map(
-          category => <Item key={category}> <Category {...category}/> </Item>
+          category => <Item key={category.id}> <Category {...category}/> </Item>
         )
       }
     </List>
